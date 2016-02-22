7c8d97891c59780b71e24af0660428d92076f66b
Dhanunjaya Lakshmi <dhanunjaya.lakshmi60@gmail.com>
2016-02-22 17:30:04 +0530

    Header page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  get 'home/land'
+  get 'land' => 'home#land', as: :land
   get 'about' => 'home#about', as: :about
   get 'login' => 'home#login', as: :login
   # You can have the root of your site routed with "root"
