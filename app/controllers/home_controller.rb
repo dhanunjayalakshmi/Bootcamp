0d95d77d6dc1172751106c9d02de9022ff161ac2
Dhanunjaya Lakshmi <dhanunjaya.lakshmi60@gmail.com>
2016-02-22 16:33:11 +0530

    Added actions and views for login and about us pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def land
   end
+
+  def about
+  end
+
+  def login
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   get 'home/land'
+  get 'about' => 'home#about', as: :about
+  get 'login' => 'home#login', as: :login
   # You can have the root of your site routed with "root"
   root 'home#land'
 
