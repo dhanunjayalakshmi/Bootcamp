c32b6bad466c01cc94ba0cacd878a4233ec0cf64
Dhanunjaya Lakshmi <dhanunjaya.lakshmi60@gmail.com>
2016-02-22 21:01:35 +0530

    Task controller and Model added

---
+++ db/migrate/20160222152329_create_tasks.rb
@@ -0,0 +1,11 @@
+class CreateTasks < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :description
+      t.integer :status, default: Task::Status::NOT_STARTED
+
+      t.timestamps null: false
+    end
+  end
+end
